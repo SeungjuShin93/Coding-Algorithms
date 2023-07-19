cb36cdd05cc4d07c5568fd2dbd5adbbdfa342513
Ted <76985204+SeungjuShin93@users.noreply.github.com>
2023-07-19 22:06:44 +0900

    [level 0] Title: 특이한 정렬, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -0,0 +1,29 @@
+function solution(numlist, n) {
+  const arr = [];
+  for (let i = 0; i < numlist.length; i++) {
+    const item = Number(numlist[i]);
+    const obj = {};
+    obj.value = item - n;
+    obj.index = i;
+    arr.push(obj);
+  }
+  let result = arr.sort(function (a, b) {
+    const aNum = Math.abs(a.value);
+    const bNum = Math.abs(b.value);
+    if (aNum === bNum) {
+      return b.value - a.value;
+    }
+    return aNum - bNum;
+  });
+  const answer = [];
+  for (let i = 0; i < result.length; i++) {
+    const item = result[i].index;
+    answer.push(numlist[item]);
+  }
+  return answer;
+}
+// 30과의 차액과 index를 가진 오브젝트를 배열에 push
+// 절대값이 가장 작은 수부터 push
+// 만약 절대값의 값이 같다면 절대값을 하지 않은 b.value - a.value로 정렬
+// 정렬된 배열의 index를 원래 numlist[index] 순서로 만든 배열을 생성
+// 그 배열을 리턴
