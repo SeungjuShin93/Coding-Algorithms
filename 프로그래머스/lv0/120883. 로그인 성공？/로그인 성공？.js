3d0a6579c887c4c5ca13913880db55d00b5adb3a
Ted <76985204+SeungjuShin93@users.noreply.github.com>
2023-07-22 11:52:05 +0900

    [level 0] Title: 로그인 성공?, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,17 @@
+function solution(id_pw, db) {
+  const obj = { ID: id_pw[0], PASSWORD: id_pw[1] };
+  let pwCheck = false;
+  for (let i = 0; i < db.length; i++) {
+    if (obj.ID === db[i][0]) {
+      if (obj.PASSWORD === db[i][1]) {
+        return 'login';
+      }
+      pwCheck = true;
+    }
+    continue;
+  }
+  if (pwCheck) {
+    return 'wrong pw';
+  }
+  return 'fail';
+}
