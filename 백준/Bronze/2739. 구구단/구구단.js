b91cbdb03f6ec32e8067fd8a2c5d5fbfe85f596e
Ted <76985204+SeungjuShin93@users.noreply.github.com>
2023-10-05 22:33:29 +0900

    [Bronze V] Title: 구구단, Time: 116 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+for (let i = 1; i <= 9; i++) {
+  console.log(`${n} * ${i} = ${n * i}`);
+}
