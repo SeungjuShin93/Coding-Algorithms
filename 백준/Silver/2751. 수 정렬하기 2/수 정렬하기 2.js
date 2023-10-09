2c4355512b2b00b167dda887603958aee76c0101
Ted <76985204+SeungjuShin93@users.noreply.github.com>
2023-10-09 18:26:51 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1452 ms, Memory: 215644 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,14 @@
+var fs = require("fs");
+var input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const n = Number(input[0]);
+const arr = [];
+for(let i=1; i<=n; i++){
+  arr.push(Number(input[i]))
+}
+arr.sort((a,b) => a-b);
+let result = "";
+for (let i=0; i<arr.length; i++){
+  result += arr[i] + "\n";
+}
+console.log(result);
