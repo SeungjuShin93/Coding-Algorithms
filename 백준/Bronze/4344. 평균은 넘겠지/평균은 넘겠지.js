7b98a6c6d428416b31e3171c5f5189e0d8742ad7
Ted <76985204+SeungjuShin93@users.noreply.github.com>
2023-10-09 17:52:07 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 128 ms, Memory: 9904 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.js
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.js
@@ -10,11 +10,8 @@ for ( let i = 1 ; i <= N; i++){
    sum += data[j];
   }
   let avg = (sum / n);
-  let cnt = 0;
-  for(let x = 1; x <=n; x++){
-    if(data[x] > avg){
-      cnt += 1;
-    }
-  }  
-  console.log((cnt / n * 100).toFixed(3) + "%");
+  data.shift();
+  let arr = data.filter((item) => item > avg);
+  
+  console.log(((arr.length / n) * 100).toFixed(3) + "%");
 }
