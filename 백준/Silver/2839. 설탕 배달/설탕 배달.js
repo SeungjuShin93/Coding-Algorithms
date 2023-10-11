29d80687d5d31d465530971f4b9d1c463527e29b
Ted <76985204+SeungjuShin93@users.noreply.github.com>
2023-10-11 15:15:28 +0900

    [Silver IV] Title: 설탕 배달, Time: 132 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -1,19 +1,20 @@
 var fs = require("fs");
 var input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let n = input[0];
+let n = Number(input[0]);
 let cnt = 0;
 
-while (n > 0) {
+while (n >= 0) {
   if (n % 5 === 0) {
     cnt += parseInt(n / 5);
     n %= 5;
-    continue;
+    console.log(cnt);
+    break;
   }
   n -= 3;
   cnt++;
 }
-if (n !== 0) {
+
+if (n < 0) {
   return console.log(-1);
 }
-console.log(cnt);
