9a157c387aad1126291caa290dd58af4024df116
Ted <76985204+SeungjuShin93@users.noreply.github.com>
2023-10-12 11:02:33 +0900

    [Silver II] Title: A → B, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.js
+++ 백준/Silver/16953. A → B/A → B.js
@@ -4,16 +4,18 @@ var input = fs.readFileSync("/dev/stdin").toString().split("\n");
 let [A, B] = input[0].split(" ").map(Number);
 let cnt = 1;
 
-while (B >= A) {
-  if (A === B) {
-    return console.log(cnt);
-  }
+while(B > A){
   cnt++;
-  if (B % 10 === 1) {
+  if(B % 10 === 1){
     B = parseInt(B / 10);
-  } else if (B % 2 === 0) {
-    B /= 2;
-  } else break;
+  } else if(B % 2 ===0){
+    B /=2 ;
+  }
+  else break;
 }
-
-console.log(-1);
+if (A === B) {
+  console.log(cnt);
+}
+else{
+  console.log(-1);
+}
