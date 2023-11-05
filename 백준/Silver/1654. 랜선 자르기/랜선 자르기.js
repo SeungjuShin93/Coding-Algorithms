ac861592257ab4ec3d0370debd70a9f9b0eba5d7
Ted <76985204+SeungjuShin93@users.noreply.github.com>
2023-11-05 16:59:50 +0900

    [Silver II] Title: 랜선 자르기, Time: 224 ms, Memory: 17128 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const [K, N] = input[0].split(' ').map(Number);
+const arr = [];
+for (i = 1; i <= K; i++) {
+  arr.push(Number(input[i]));
+}
+let maxLength = 0;
+
+let start = 0;
+let end = arr.reduce((a, b) => Math.max(a, b));
+while (start <= end) {
+  let cnt = 0;
+  let mid = parseInt((start + end) / 2);
+  for (x of arr) {
+    cnt += parseInt(x / mid);
+  }
+  if (cnt < N) {
+    end = mid - 1;
+  }
+  else {
+    maxLength = mid;
+    start = mid + 1;
+  }
+}
+
+console.log(maxLength);
